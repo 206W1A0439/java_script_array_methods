f34cefefd4be4c2d9ce25534ae8fa03b886acf6e
Kalyani Pebbuli <116427058+206W1A0439@users.noreply.github.com>
2025-06-16 19:40:02 +0530

    Update sum_of_num.js

--- sum_of_num.js
+++ sum_of_num.js
@@ -1,7 +1,15 @@
+document.body.style.textAlign="center"
+document.body.style.backgroundColor="pink"
+document.body.style.marginTop="150px"
+h1=document.createElement("h1")
+h1.innerText="Sum of Two Numbers"
+document.body.append(h1)
 in1=document.createElement("input")
 in1.type="number"
 in1.id="num1"
-in1.style.width="100px"
+in1.style.width="250px"
+in1.style.height="30px"
+
 in1.value=0
 in1.style.marginBottom="10px"
 document.body.append(in1)
@@ -13,7 +21,8 @@ in2.type="number"
 in2.id="num2"
 in2.value=0
 in2.style.marginBottom="10px"
-in2.style.width="100px"
+in2.style.width="250px"
+in2.style.height="30px"
 document.body.append(in2)
 document.body.append(document.createElement("br"))
 
@@ -21,6 +30,9 @@ out=document.createElement("button")
 out.id="sum"
 out.innerText="Add_Numbers"
 out.style.marginBottom="10px"
+out.style.width="100px"
+out.style.height="30px"
+out.style.backgroundColor="lightblue"
 
 document.body.append(out)
 document.body.append(document.createElement("br"))
@@ -29,9 +41,10 @@ document.body.append(document.createElement("br"))
 ans=document.createElement("input")
 ans.type="text"
 ans.id="ans"
-ans.style.width="100px"
 document.body.append(ans)
 ans.placeholder="Result"
+ans.style.width="250px"
+ans.style.height="30px"
 
 out.addEventListener("click",()=>{
     let num1=in1.value
